cd4c953982875328bbb303be35fe2ed1bb626bd9
Simone Cheah <simone_cheah@hotmail.com>
2020-11-04 20:31:15 +1100

    initial db set up

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE database burgers_db;
+use burgers_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+values ('Cheeseburger', false), ('Hamburger', false), ('Angus beef burger', false), ('Vegetarian burger', true), ('Pineapple and beetroot sunshine burger', true);
