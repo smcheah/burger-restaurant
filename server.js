50e32aac40fe91f228aa2bfdb9c261d010ca8b45
Simone Cheah <simone_cheah@hotmail.com>
2020-11-05 04:56:12 +1100

    added connection to server using express

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const express = require('express');
+
+let PORT = process.env.PORT || 8000;
+let app = express();
+
+// app.use(express.static("public"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+let template = require('express-handlebars');
+
+app.engine('handlebars', template({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+let routes = require('./controllers/burgers_controller.js');
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log('connected on http://localhost:' + PORT)
+});
