4d91226ecaa663a1a4a724252a2f9c3d99caf163
Simone Cheah <simone_cheah@hotmail.com>
2020-11-08 04:21:12 +1100

    added function for submitting a burger and updating to devoured

---
+++ public/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+    $('.devour-burger').on('click', (event) => {
+        // event.preventDefault();
+        
+        let id = event.target.dataset.id;
+
+        let toggleDevoured = {
+            devoured: true
+        };
+
+        $.ajax('/api/burgers/' + id, {
+            type: 'PUT',
+            data: toggleDevoured
+        }).then( function() {
+            console.log('changed devoured to ' + toggleDevoured);
+            location.reload();
+        })
+    })
+
+    $('.add-burger').on('submit', function(event) {
+        event.preventDefault();
+        
+        let newBurger = {
+            burger_name: $('#burger-name').val().trim()
+        }
+
+        $.ajax('/api/burgers', {
+            type: 'POST',
+            data: newBurger
+        }).then(function() {
+            console.log('created new burger: ' + newBurger);
+            location.reload();
+        })
+    })
+})
